fbc85b0cd74d1bb4e473897cc6b227b40b7cf2f1
Anta <antambaye261971@gmail.com>
2022-09-21 12:15:42 +0000

    Making routes APi

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\OffreController;
+use App\Http\Controllers\ProjetController;
+use App\Http\Controllers\QuartierController;
+use App\Http\Controllers\ResponsableController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +21,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::resource('offres', OffreController::class);
+Route::resource('projets', ProjetController::class);
+Route::resource('quartiers',QuartierController::class);
+Route::resource('regions', RegionController::class);
+Route::resource('communes', CommuneController::class);
+Route::resource('departements', DepartementController::class);
+Route::resource('responsables',ResponsableController::class);
+Route::resource('secteurs', secteursController::class);
